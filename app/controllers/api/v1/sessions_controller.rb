e9e8e14669de6a3d809cf5483a2d99e4c41e89e8
Hijikata1869 <liv.knk@gmail.com>
2026-06-21 13:13:05 +0900

    feat: セッション作成のエンドポイントを追加

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    if user = User.authenticate_by(email: params[:email], password: params[:password])
+      session[:user_id] = user.id
+      render json: { message: "ログインしました" }, status: :ok
+    else
+      render json: { message: "ログインできませんでした" }, status: :unauthorized
+    end
+  end
+end
