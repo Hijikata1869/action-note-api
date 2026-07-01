4523475550e884f0b3b7a58a8430fc5d95d93a73
Hijikata1869 <liv.knk@gmail.com>
2026-07-01 20:06:39 +0900

    feat: application_controller.rbにauthenticate_userメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::API
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
+
+    def authenticate_user!
+      unless current_user
+        render json: { message: "ログインしていません" }, status: :unauthorized
+      end
+    end
 end
