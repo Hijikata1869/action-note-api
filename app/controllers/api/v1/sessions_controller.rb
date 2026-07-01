db562bfd1f8d428500eaa90226881f68d7c48099
Hijikata1869 <liv.knk@gmail.com>
2026-07-01 20:13:12 +0900

    fix: sessions_controllerのdestroyとshowアクションをログイン中のユーザーのみ行えるよう修正

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :authenticate_user!, only: [ :destroy, :show ]
   def create
     if user = User.authenticate_by(email: params[:email], password: params[:password])
       reset_session
@@ -15,10 +16,6 @@ class Api::V1::SessionsController < ApplicationController
   end
 
   def show
-    if current_user
-      render json: { current_user: current_user.safe_attributes }, status: :ok
-    else
-      render json: { message: "ログインしていません" }, status: :unauthorized
-    end
+    render json: { current_user: current_user.safe_attributes }, status: :ok
   end
 end
