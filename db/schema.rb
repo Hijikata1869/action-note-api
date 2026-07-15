5255c3197f1b292bbd3f8bb034f06134bf7cc4f8
Hijikata1869 <liv.knk@gmail.com>
2026-07-15 15:02:01 +0900

    feat: booksテーブルにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_12_042417) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_055940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "author", null: false
     t.datetime "created_at", null: false
+    t.integer "status", default: 0, null: false
     t.string "title", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
