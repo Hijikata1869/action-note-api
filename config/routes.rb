3005abfe634b2266dd4e9291f8aa9f93a426ddf9
Hijikata1869 <liv.knk@gmail.com>
2026-07-16 14:29:13 +0900

    feat: 書籍CRUDのためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resource :session, only: [ :create, :destroy, :show ]
+      resources :books
     end
   end
 end
