d16afef85529d24f26da8a679a1a4ba89533b3ec
Hijikata1869 <liv.knk@gmail.com>
2026-07-16 20:31:37 +0900

    feat: 書籍情報更新機能を追加

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_book, only: [ :update, :destroy ]
 
   def create
     book = current_user.books.build(book_params)
@@ -10,14 +11,25 @@ class Api::V1::BooksController < ApplicationController
     end
   end
 
+  def update
+    if @book.update(book_params)
+      render json: @book, status: :ok
+    else
+      render json: { errors: @book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def destroy
-    book = current_user.books.find(params[:id])
-    book.destroy!
+    @book.destroy!
     render json: { message: "書籍を削除しました" }, status: :ok
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :author, :status)
+  end
+
+  def set_book
+    @book = current_user.books.find(params[:id])
   end
 end
