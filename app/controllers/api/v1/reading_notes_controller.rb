9aea5f713dc165997364e968bf9e8fd28462ec49
Hijikata1869 <liv.knk@gmail.com>
2026-07-29 16:36:39 +0900

    feat: reading_notes_controllerにcreateメソッドを追加

---
+++ app/controllers/api/v1/reading_notes_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ReadingNotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    book = current_user.books.find(params[:book_id])
+    reading_note = ReadingNote.new(reading_note_params.merge(book_id: book.id))
+    if reading_note.save
+      render json: reading_note, status: :created
+    else
+      render json: { errors: reading_note.errors.full_messages }, status: :unprocessable_content
+    end
+  end
+
+  private
+  def reading_note_params
+    params.require(:reading_note).permit(:content)
+  end
+end
