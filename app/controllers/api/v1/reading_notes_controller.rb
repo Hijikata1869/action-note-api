f162b3d9cc64e8f1943f46d4b71c02963b611ca8
Hijikata1869 <liv.knk@gmail.com>
2026-07-29 17:28:17 +0900

    feat: 読書メモの更新機能を追加

--- app/controllers/api/v1/reading_notes_controller.rb
+++ app/controllers/api/v1/reading_notes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReadingNotesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_reading_note, only: %i[show update]
 
   def create
     book = current_user.books.find(params[:book_id])
@@ -12,12 +13,23 @@ class Api::V1::ReadingNotesController < ApplicationController
   end
 
   def show
-    reading_note = current_user.reading_notes.find(params[:id])
-    render json: reading_note, status: :ok
+    render json: @reading_note, status: :ok
+  end
+
+  def update
+    if @reading_note.update(reading_note_params)
+      render json: @reading_note, status: :ok
+    else
+      render json: { errors: @reading_note.errors.full_messages }, status: :unprocessable_content
+    end
   end
 
   private
   def reading_note_params
     params.require(:reading_note).permit(:content)
+  end
+
+  def set_reading_note
+    @reading_note = current_user.reading_notes.find(params[:id])
   end
 end
